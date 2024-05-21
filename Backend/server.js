9c393ded07f3c948cc2f037ea649e6723e8b9121
Mohamed Ibrahim <sallamm733@gmail.com>
2024-05-21 12:58:24 +0300

    mohamed and omar backend for regester

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,53 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const connectDB = require('./connection-db');
+const User = require('./models/user');
 
 dotenv.config();
+
+connectDB().then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.log(err);
+  process.exit(1);
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+app.post('/register', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({ msg: 'User already exists' });
+    }
+
+    user = new User({
+      username,
+      email,
+      password
+    });
+
+    await user.save();
+
+    res.send('User registered succssfully!');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

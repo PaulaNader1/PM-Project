3008751a4a5838cd2d2d408e14784d547fc81f3d
PaulaNader1 <128845194+PaulaNader1@users.noreply.github.com>
2024-05-23 21:36:30 +0300

    .

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -3,28 +3,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-<<<<<<< HEAD
-const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    profile: {
-        bio: String, // Add bio field
-        avatar: String, // Add avatar field
-    },
-});
-=======
 const userSchema = new Schema({
   email: {
     type: String,
@@ -57,7 +35,7 @@ const userSchema = new Schema({
     type: String,
   },
 }, { timestamps: true });
->>>>>>> 1291c302ca1c3c5aed5c0153688151372c402556
+
 
 const User = mongoose.model('User', userSchema);
 
